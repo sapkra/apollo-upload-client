84e29ef5515fd3b1e81730745386256e7d68d992
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-04-27 10:24:38 -0400

    copy rewriteToURIForGet over

--- src/index.js
+++ src/index.js
@@ -11,13 +11,58 @@ const {
   fromError,
 } = require('@apollo/client');
 const {
-  rewriteURIForGET,
-} = require('@apollo/client/link/http/rewriteURIForGET');
-const {
   extractFiles,
   isExtractableFile,
   ReactNativeFile,
 } = require('extract-files');
+
+// eslint-disable-next-line
+function rewriteURIForGET(chosenURI, body) {
+  var queryParams = [];
+  var addQueryParam = function (key, value) {
+    queryParams.push(key + '=' + encodeURIComponent(value));
+  };
+  if ('query' in body) addQueryParam('query', body.query);
+
+  if (body.operationName) addQueryParam('operationName', body.operationName);
+
+  if (body.variables) {
+    var serializedVariables = void 0;
+    try {
+      serializedVariables = serializeFetchParameter(
+        body.variables,
+        'Variables map'
+      );
+    } catch (parseError) {
+      return { parseError };
+    }
+    addQueryParam('variables', serializedVariables);
+  }
+  if (body.extensions) {
+    var serializedExtensions = void 0;
+    try {
+      serializedExtensions = serializeFetchParameter(
+        body.extensions,
+        'Extensions map'
+      );
+    } catch (parseError) {
+      return { parseError };
+    }
+    addQueryParam('extensions', serializedExtensions);
+  }
+  var fragment = '',
+    preFragment = chosenURI;
+  var fragmentStart = chosenURI.indexOf('#');
+  if (fragmentStart !== -1) {
+    fragment = chosenURI.substr(fragmentStart);
+    preFragment = chosenURI.substr(0, fragmentStart);
+  }
+  var queryParamsPrefix = preFragment.indexOf('?') === -1 ? '?' : '&';
+  var newURI =
+    preFragment + queryParamsPrefix + queryParams.join('&') + fragment;
+  return { newURI };
+}
+
 /**
  * A React Native [`File`](https://developer.mozilla.org/en-US/docs/Web/API/File)
  * substitute.
